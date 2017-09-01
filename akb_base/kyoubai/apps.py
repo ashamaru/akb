8ae571dcedfc671e8bb8c7cf661725f1bc207eed
ThomasHildebrand <thomas1.hildebrand@st.oth-regensburg.de>
2017-09-02 00:18:43 +0200

    working_common: Added app kyoubai in akb_base.

---
+++ akb_base/kyoubai/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KyoubaiConfig(AppConfig):
+    name = 'kyoubai'
---
+++ akb_base/kyoubai/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Position(models.Model):
+    id_key = models.IntegerField('Identification Key', primary_key=True)
+    pos_name = models.CharField(max_length=100)
+    pos_desc = models.CharField(max_length=1000)
+    price_min = models.IntegerField()
+    price_growth = models.IntegerField()
+    price_start = models.IntegerField()
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField()
+    state = models.CharField(max_length=100, default='used')
+    pictures = models.CharField(max_length=100, default='prototyp')
+
